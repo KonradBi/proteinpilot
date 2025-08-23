9422d3e373d7de0f8460cbd9a3b632891a8a56c7
KonradBi <konradbierwagen@gmail.com>
2025-08-23 19:04:09 +0800

    Initial Commit

---
+++ ProteinPilot/ProteinPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProteinPilotApp.swift
+//  ProteinPilot
+//
+//  Created by Konrad on 23.08.25.
+//
+
+import SwiftUI
+
+@main
+struct ProteinPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProteinPilotTests/ProteinPilotTests.swift
@@ -0,0 +1,16 @@
+//
+//  ProteinPilotTests.swift
+//  ProteinPilotTests
+//
+//  Created by Konrad on 23.08.25.
+//
+
+import Testing
+
+struct ProteinPilotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
